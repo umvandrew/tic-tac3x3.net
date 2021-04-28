656c3b8c2e6e1ad79ba434c27dd958475423b9f6
ANDREW-NB\numva <numv.andrew@gmail.com>
2021-04-28 21:48:25 +0300

    оо

---
+++ tic-tac-3x3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace tic_tac_3x3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.label3.Location = new System.Drawing.Point(294, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 23);
+            this.label3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button1.Location = new System.Drawing.Point(35, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "СТАРТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.textBox2.Location = new System.Drawing.Point(148, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 38);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.textBox1.Location = new System.Drawing.Point(35, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 38);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.label2.Location = new System.Drawing.Point(135, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " Нолики";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.label1.Location = new System.Drawing.Point(9, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Крестики ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        #endregion
+    }
+}
---
+++ tic-tac-3x3/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_3x3
+{public partial class Form1 : Form
+    {
+        
+        //базовые постоянные, отступы, пазмер клеток
+        public static float xstart = 50, ystart = 50, step = 50;
+        //"поля" с отметками ходов
+        public static int[,] littleMap = new int [3, 3];
+
+        public static int[,] winComb = new int[3, 2]; //начальная и конечная точка выйгрышной комбинации 
+
+        public static int[] countWin = new int[2] {0,0};
+        //переменная определяет чей сейчас ход: true - крестики, false - нолики
+        public bool turn = true;
+        public bool isPlaying = false;
+
+        public static int winx, winy, winvx, winvy;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            //рисуем поле 
+            for (int i = 50; i <= 200; i+=50)
+            {
+                g.DrawLine(new Pen(Color.Black, 2f), i, xstart, i, 200);
+                g.DrawLine(new Pen(Color.Black, 2f), 200, i, ystart, i);
+            }
+            
+            //рисуем крестик/нолик, если клик мышкой был
+            if (isPlaying)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for ( int j = 0; j < 3; j++)
+                    {
+                        if (littleMap[i,j]==1)
+                        {
+                            g.DrawLine(new Pen(Color.Black, 2f), xstart + 5 + i * step, ystart + 5 + j * step,
+                                xstart - 5 + (i + 1) * step, ystart + 5 + j * step + 40);
+                            g.DrawLine(new Pen(Color.Black, 2f), xstart + 5 + i * step, ystart + 5 + j * step + 40,
+                                xstart + 5 + i * step + 40, ystart + 5 + j * step);
+
+                        }
+                        if (littleMap[i, j] == 2)
+                        {
+                            g.DrawEllipse(new Pen(Color.Red, 2f), xstart + 5 + i * step, ystart + 5 + j * step, 40, 40);
+                        
+                        }
+                    }
+                }
+            }
+            
+            //проверка на победу 
+            for (int c = 1; c <= 2 ; c++)
+            {
+                if (CheckWin(c))
+                {
+                    g.DrawLine(new Pen(Color.Orange, 5f), xstart + 25 + (winComb[0, 0] * step), ystart + 25 + (winComb[0, 1] * step), 
+                        xstart + 25 + (winComb[2, 0] * step), ystart + 25 + (winComb[2, 1] * step ));
+                    // g.DrawLine(new Pen(Color.Aqua, 3f), xstart + 25 + (winy + winvy) * step, ystart + 25 + (winx + winvx) * step, 
+                    //   xstart + 25 + (winy + 2 * winvy) * step, ystart + 25 + (winx + 2*winvx) * step);
+
+                    ClearMap();
+                    
+                    countWin[c-1] += 1;
+                    MessageBox.Show("Победа "+c+" игрока") ;
+                    if (c == 1) textBox1.Text = (countWin[0]).ToString();
+                    if (c == 2) textBox2.Text = (countWin[1]).ToString();
+
+                }
+            }
+
+            //ничья
+            if (CheckMap())
+            {
+                MessageBox.Show("Нет победителя");
+                ClearMap();
+            }
+        }
+
+        //проверка победы 
+        private static bool CheckWin(int c)
+        {
+            for (var i = 0; i < 3; i++) 
+            {
+                if (CheckLine(0, i, 1, 0, c)) // проверим линию по y
+                {
+                    winx = 0;
+                    winy = i;
+                    winvx = 1;
+                    winvy = 0;
+                    return true;   
+                }
+                if(CheckLine(i, 0, 0, 1, c)) // проверим линию по х 
+                {
+                    winx = i;
+                    winy = 0;
+                    winvx = 0;
+                    winvy = 1;
+                    return true;   
+                }
+            }
+            if (CheckLine(0, 0, 1, 1,  c)) // проверим по диагонали х -у 
+            {
+                winx = 0;
+                winy = 0;
+                winvx = 1;
+                winvy = 1;
+                return true;   
+            }
+            if (CheckLine(0, 2, 1, -1,  c)) // проверим по диагонали х -у 
+            {
+                winx = 0;
+                winy = 2;
+                winvx = 1;
+                winvy = -1;
+                return true;   
+            }
+            return false;
+        }
+        
+        // проверка линии
+        private static bool CheckLine(int x, int y, int vx, int vy, int c)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                if (littleMap[(y + i * vy), (x + i * vx)] == c)
+                {
+                    winComb[i, 0] = (y + i * vy);
+                    winComb[i, 1] = (x + i * vx);
+                }
+
+                if (littleMap[(y + i * vy), (x + i * vx)] != c) return false; // проверим одинаковые-ли символы в ячейки
+            }
+            return true;
+        }
+        
+        //проверка на пустые клетки(заполненность поля)
+        private static bool CheckMap()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (littleMap[i, j] == 0) return false;
+                }
+            }
+
+            return true;
+        }
+
+        //Очистить поле, подготовиться к следующей игре 
+        private void ClearMap()
+        {
+             
+            for (var i = 0; i < 3; i++) 
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    littleMap[i, j] = 0;
+                }
+            }
+            
+            isPlaying = false;
+            turn = true;
+            button1.Text = "Играсть снова";
+            button1.BackColor = Color.Gold;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isPlaying)
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        if ((e.X > xstart + i * step)
+                            && (e.X < xstart + (i + 1) * step)
+                            && (e.Y > ystart + j * step)
+                            && (e.Y < ystart + (j + 1) * step))
+                        {
+                            if ((littleMap[i, j] != 1) && (littleMap[i, j] != 2))
+                            {
+                                if (turn)
+                                {
+                                    littleMap[i, j] = 1;
+                                    turn = false;
+                                }
+                                else if (turn == false)
+                                {
+                                    littleMap[i, j] = 2;
+                                    turn = true;
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Клетка занята, выберите другую!");
+                            }
+                        }
+
+                        Invalidate();
+                    }
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isPlaying = true;
+            button1.Text = "Идет игра";
+            button1.BackColor = Color.Chartreuse;
+            Invalidate();
+        }
+
+    }
+}
